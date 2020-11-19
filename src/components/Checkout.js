64249e37c53e41a1023a0f36f624f45d7e9522a7
Izzy Bolt <izzy@VIV-ID-MBA-04.local>
2020-11-19 22:23:53 +0000

    thankyou

--- src/components/Checkout.js
+++ src/components/Checkout.js
@@ -209,10 +209,10 @@ class Checkout extends Component {
                                          </div>
                                 </form>     
                                 <div className="thankyou">
-                                    <h2 >{this.state.thankYou}</h2>
-                                    <h1>{this.state.h1}</h1>
-                                    <h2>{this.state.h2}</h2>
-                                    <h2>{this.state.orderNumber}</h2>
+                                    <p >{this.state.thankYou}</p>
+                                    <p>{this.state.h1}</p>
+                                    <p>{this.state.h2}</p>
+                                    <p>{this.state.orderNumber}</p>
                             </div>
 
 
