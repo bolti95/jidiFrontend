f7b6cadb7c849b30abc611660d7441e637be519b
Izzy Bolt <izzy@VIV-ID-MBA-04.local>
2020-11-19 10:36:40 +0000

    changed login

--- src/components/Login.js
+++ src/components/Login.js
@@ -14,7 +14,7 @@ class Login extends Component {
             userName: '',
             password: '',
             loggedIn: '',
-            logout: 'logout'
+            logout: 'login'
         }
     }
 
