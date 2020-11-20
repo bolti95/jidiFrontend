a52d6eb1e0e35eec327f9043b1c5fe662da43956
Izzy Bolt <izzy@VIV-ID-MBA-04.local>
2020-11-20 11:23:04 +0000

    changing logout action

--- src/reducers/basketReducer.js
+++ src/reducers/basketReducer.js
@@ -171,6 +171,7 @@ export default (state = initialState, action) => {
                 return {
                     
                     // ...state,
+                    ...state,
                     logout:  'logout'
 
                 }
